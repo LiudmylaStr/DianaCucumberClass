bb377a90e3014c99283a3b54433b71c4a3890539
iy_r <iyengwork@gmail.com>
2024-08-19 17:27:22 -0500

    Initial commit

---
+++ src/test/java/io/classdiana/runner/CukesRunner.java
@@ -0,0 +1,24 @@
+package io.loop.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+            "pretty",
+            "html:target/html-reports/cucumber-report.html",
+            "json:target/json-reports/json-report.json",
+            "rerun:target/rerun.txt",
+            "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        features = "src/test/resources/features",
+        glue = "io/loop/step_definitions",
+        dryRun = false,
+        tags = "@moreSoftAssertion",
+        monochrome = false,
+        publish = false
+)
+public class CukesRunner {
+}
---
+++ src/test/java/io/classdiana/utilities/Driver.java
@@ -0,0 +1,71 @@
+package io.loop.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    /*
+    Creating the private constructor so this class's object is not reachable from outside
+     */
+
+    private Driver(){}
+
+    /*
+    making driver instance private
+    static - run before everything else and use ins static method
+     */
+
+//    private static WebDriver driver;
+    //implement threadLocal to achive multi thread
+    public static InheritableThreadLocal <WebDriver> driverPool = new InheritableThreadLocal<>();
+
+    /*
+    reusable method that will return the same driver instance everytime called
+     */
+
+    /**
+     * singleton patter
+     * @return
+     */
+    public static WebDriver getDriver(){
+        if(driverPool.get()==null){
+            String browserType = ConfigurationReader.getProperties("browser");
+            switch (browserType.toLowerCase()){
+                case "chrome":
+                    driverPool.set(new ChromeDriver());
+                    break;
+                case "firefox":
+                    driverPool.set(new FirefoxDriver());
+                    break;
+                case "safari":
+                    driverPool.set(new SafariDriver());
+                    break;
+                case "headless":
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--headless");
+                    driverPool.set(new ChromeDriver(options));
+            }
+            driverPool.get().manage().window().maximize();
+            driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+        return driverPool.get();
+    }
+
+    /**
+     * closing driver
+     * @author nadir
+     */
+    public static void closeDriver(){
+        if(driverPool.get() !=null){
+            driverPool.get().quit();
+//            driverPool.get() = null;
+            driverPool.remove();
+        }
+    }
+}
